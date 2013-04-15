d87d514d14b8095ccdd7d1bf5f383d94d7fa285f
Sergey Vinokurov <serg.foo@gmail.com>
2013-04-15 22:56:43 +0300

    tweaked project.clj; tweaked draw-grid; defined adjust-stroke-width and with-saved-matrix;

--- project.clj
+++ project.clj
@@ -9,10 +9,17 @@
 
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
+  :javac-options     ["-target" "1.6" "-source" "1.6"]
 
   ;; :local-repo "/home/sergey/.m2/repository/"
   :repositories [["local" "file:///home/sergey/.m2/repository/"]]
+  :plugins [[lein-droid "0.1.0-preview2-enhanced-dex"]]
   :dependencies [[android/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.1"]]
                    :android {:aot :all-with-unused}}
-             :release {:android {:aot :all}}})
+             :release {:android {:aot :all}}}
+  :android {:sdk-path "/home/sergey/projects/android/android-sdk-linux"})
+
+;; Local Variables:
+;; clojure-compile/lein-command: "lein with-profiles %s do compile, install"
+;; End:
--- src/clojure/android/clojure/graphic.clj
+++ src/clojure/android/clojure/graphic.clj
@@ -19,16 +19,48 @@
   ([alpha red green blue]
      (color->paint (Color/argb alpha red green blue))))
 
-(defn draw-grid [^Canvas canvas
-                 n
-                 ^Paint paint]
+(defn adjust-stroke-width ^Paint [^Paint p
+                                  new-width]
+  (let [new-paint (Paint. p)]
+    (.setStrokeWidth new-paint new-width)
+    new-paint))
+
+(defmacro with-saved-matrix [canvas-var & body]
+  `(try
+     (.save ^Canvas ~canvas-var Canvas/MATRIX_SAVE_FLAG)
+     ~@body
+     (finally
+       (.restore ^Canvas ~canvas-var))))
+
+(defn draw-grid
+  ^{:pre [(not (nil? canvas))
+          (not (nil? paint))
+          (not (nil? label-paint))]}
+  [^Canvas canvas
+   x-labels
+   y-labels
+   ^Paint paint
+   ^Paint label-paint]
   (let [width  (.getWidth canvas)
         height (.getHeight canvas)
-        dx (/ width n)
-        dy (/ height n)]
-    (loop [i 0]
-      (.drawLine canvas (* i dx) 0        (* i dx) height   paint)
-      (.drawLine canvas 0        (* i dy) width    (* i dy) paint)
-      (when (not= i n)
-        (recur (+ i 1))))))
+        dx (/ width (dec (count x-labels)))
+        dy (/ height (dec (count y-labels)))
+        x-label-paint (let [p (Paint. label-paint)]
+                        (.setTextAlign p android.graphics.Paint$Align/CENTER)
+                        p)
+        y-label-paint (let [p (Paint. label-paint)]
+                        (.setTextAlign p android.graphics.Paint$Align/RIGHT)
+                        p)]
+    (loop [i 0
+           x-labels (seq x-labels)]
+      (when x-labels
+        (.drawLine canvas (* i dx) 0 (* i dx) height paint)
+        (.drawText canvas (first x-labels) (* i dx) 0 x-label-paint)
+        (recur (inc i) (next x-labels))))
+    (loop [i 0
+           y-labels (seq y-labels)]
+      (when y-labels
+        (.drawLine canvas 0 (* i dy) width (* i dy) paint)
+        (.drawText canvas (first y-labels) 0 (* i dy) y-label-paint)
+        (recur (inc i) (next y-labels))))))
 
